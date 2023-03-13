6af23f41d4ecc1703ff369d04219657b94f037de
RedevsSKHA <113835061+redsamedev@users.noreply.github.com>
2023-03-13 16:50:50 +0100

    Scripts

---
+++ Assets/Scripts/Retry.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Retry : MonoBehaviour
+{
+    public AudioSource Click;
+   public void Reset()
+   {
+    SceneManager.LoadScene("TutuMap");
+    Click.Play();
+   }
+}
